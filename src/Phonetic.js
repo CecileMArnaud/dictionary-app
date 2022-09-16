3c90f4b4e1c49c5958c0802bf354db98ac2a460a
CecileMArnaud <104260276+CecileMArnaud@users.noreply.github.com>
2022-09-16 18:42:33 +0200

    tweaked meaning CSS

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,10 +4,8 @@ export default function Phonetic(props) {
 	if (props.phonetic.text) {
 		return (
 			<div className="phoentic">
-				<div className="audio-phonetic">
-					<a href={props.phonetic.audio}>Listen</a>
-				</div>
-				<div className="text-phonetic">{props.phonetic.text}</div>
+				<a href={props.phonetic.audio}>Listen </a>
+				<span>{props.phonetic.text}</span>
 			</div>
 		);
 	}
