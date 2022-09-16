feb1785658feb371f22332385ed4f0e81eaae6ca
CecileMArnaud <104260276+CecileMArnaud@users.noreply.github.com>
2022-09-16 19:40:38 +0200

    added image API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results.js";
+import Images from "./Images.js";
 
 export default function Dictionary() {
 	let [query, setQuery] = useState(null);
 	let [results, setResults] = useState(null);
+	let [images, setImages] = useState(null);
+
 	function search(event) {
 		event.preventDefault();
 		let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
 		axios.get(apiURL).then(showDefinition);
+
+		let pexelsApiKey =
+			"563492ad6f91700001000001aa42f359e76542b78155e06c39a40c33";
+		let pexelsApiURL = `https://api.pexels.com/v1/search?query=${query}&per_page=8`;
+		axios
+			.get(pexelsApiURL, {
+				headers: { Authorization: `Bearer ${pexelsApiKey}` },
+			})
+			.then(getImages);
+	}
+
+	function getImages(response) {
+		setImages(response.data.photos);
 	}
 
 	function showDefinition(response) {
@@ -28,6 +44,7 @@ export default function Dictionary() {
 				</form>
 			</section>
 			<Results results={results} />
+			<Images images={images} />
 		</div>
 	);
 }
---
+++ src/Images.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Images(props) {
+	if (props.images) {
+		console.log(props.images);
+		return (
+			<section className="imageGallery">
+				<div className="row">
+					{props.images.map(function (image, index) {
+						return (
+							<div className="col-md-3 col-sm-6">
+								<a href={image.src.original}>
+									<img
+										src={image.src.tiny}
+										alt={image.alt}
+										className="img-fluid"
+									/>
+								</a>
+							</div>
+						);
+					})}
+				</div>
+			</section>
+		);
+	} else {
+		return null;
+	}
+}
