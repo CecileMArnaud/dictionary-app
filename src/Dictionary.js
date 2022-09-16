3b7452672c7e6f60ac17aca6d3a21de7780bbc7b
CecileMArnaud <104260276+CecileMArnaud@users.noreply.github.com>
2022-09-16 14:22:51 +0200

    connected to dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
 	let [query, setQuery] = useState(null);
 	function search(event) {
 		event.preventDefault();
-		alert(`searching ${query}`);
+		let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
+		axios.get(apiURL).then(showDefinition);
 	}
+
+	function showDefinition(response) {
+		console.log(response.data[0]);
+	}
+
 	function getQuery(event) {
 		setQuery(event.target.value);
 	}
